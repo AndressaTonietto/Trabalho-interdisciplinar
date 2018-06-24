57b070dc5c6bf5380fe70ac2c6cf98ea7e555100
Andressa Tonietto <andressatreis@gmail.com>
2018-06-24 19:48:38 -0300

    Add event listener on search

--- main.js
+++ main.js
@@ -1,13 +1,11 @@
+// Declare all variables
 const btn = document.querySelector('.search');
-
-btn.addEventListener('click', () => {
-    const search = document.querySelector('.input').value;
-    window.location = `http://google.com/search?q= ${search}`;
-})
-
 const button = document.querySelector('.login');
 const dropdown = document.querySelector('.dropdown');
+const logoutBtn = document.querySelector('.logout');
 const name = document.querySelector('.name');
+const saveBtn = document.querySelector('#save');
+const searchInput = document.querySelector('.search-input');
 
 const hide = () => {
     const user = localStorage.getItem('user');
@@ -20,20 +18,28 @@ const hide = () => {
         dropdown.hidden = true;
     }
 }
-
 window.onload = hide();
 
-const saveBtn = document.querySelector('#save');
-const logoutBtn = document.querySelector('.logout');
-
-logoutBtn.addEventListener('click', () => {
-    localStorage.removeItem('user');
-    hide();
+const makeSearch = () => {
+    const search = document.querySelector('.input').value;
+    window.location = `http://google.com/search?q= ${search}`;
+}
+btn.addEventListener('click', () => {
+    makeSearch();
+})
+searchInput.addEventListener('keyup', (event) => {
+    if (event.keyCode === 13) {
+        makeSearch();
+    }
 })
 
 saveBtn.addEventListener('click', () => {
     $('#loginModal').modal('hide');
     const inputName = document.querySelector('#name').value;
     localStorage.setItem('user', inputName);
+    hide();
+})
+logoutBtn.addEventListener('click', () => {
+    localStorage.removeItem('user');
     hide();
 })
